931a23fb21a6e98e2844982a465c1f9bae4e13b4
Benjamin Grap <benjamin.grap@msgq.de>
2019-04-28 17:54:59 +0200

    Initial commit of Netatmo collection script

---
+++ netatmo.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import time
+import json
+import requests
+import logging
+
+
+class netatmo:
+
+    def __init__(self, authtoken):
+        self.authtoken = authtoken
+
+
+    def getmeasure_current(self, device_id, module_id, measure, scale='max'):
+        current_time = int(time.time())
+        headers = {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Authorization': 'Bearer {}'.format(self.authtoken),
+        }
+        data = {
+            'date_begin': '{}'.format(current_time-1200),
+            'date_end': '{}'.format(current_time),
+            'device_id': device_id,
+            'module_id': module_id,
+            'scale':scale,
+            'type':measure
+        }
+
+        json_string = json.dumps(data)
+        try:
+            response = requests.post('https://app.netatmo.net/api/getmeasure', headers=headers, data=json_string)
+            if(response.status_code!=200):
+                logging.warning('Invalid HTTP status code from API')
+                return None
+        except Exception as error:
+            logging.warning('HTTP error:', error)
+            return None
+        
+        if not (response.json):
+            logging.warning('API did not return valid JSON')
+            return None
+
+        try:
+            body_list = response.json().get("body",[])
+            if not (len(body_list)):
+                return None
+        except Exception as error:
+            logging.warning('body element not found in JSON:', error)
+        
+        try:
+            values = body_list[-1].get("value")
+            value = [0.0,0.0,0.0]
+            for c_val in values:
+                for i,x in enumerate(c_val):
+                    if x is not None:
+                        value[i] = value[i] + x
+                    else:
+                        return None
+            value = list(map(lambda x: float(x)/float(len(values)),value))
+            return value
+        except Exception as error:
+            logging.warning('Unexpected value error:', error)
+
+
---
+++ weather.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import logging
+import time
+import sched
+import argparse
+import yaml
+from influxdb import influxdb
+from netatmo import netatmo
+
+def request_nodes(sc):
+    logging.info('Requesting and adding...')
+    for node in myconfig['netatmo']['nodes']:
+        if(node.get('rain_module_id',None)):
+            rain = mynetatmo.getmeasure_current(node.get('device_id'),node.get('rain_module_id'),'Rain')
+            if(rain):
+                myinfluxdb.add_measure({'mac':node.get('device_id')},"rain","{:.1f}".format(rain[0]))
+
+        temp_humid = mynetatmo.getmeasure_current(node.get('device_id'),node.get('temp_module_id'),'Temperature,Humidity')
+        if(temp_humid):
+            myinfluxdb.add_measure({'mac':node.get('device_id')},"temperature","{:.1f}".format(temp_humid[0]))
+            myinfluxdb.add_measure({'mac':node.get('device_id')},"humidity","{:.1f}".format(temp_humid[1]))
+        pressure = mynetatmo.getmeasure_current(node.get('device_id'),node.get('device_id'),'Pressure')
+        if(pressure):
+            myinfluxdb.add_measure({'mac':node.get('device_id')},"pressure","{:.1f}".format(pressure[0]))
+    
+    myinfluxdb.write_influxdb()
+    myscheduler.enter(30,1,request_nodes, (sc,))
+
+
+def read_config(configfile):
+    try:
+        with open(configfile, 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+            return cfg
+    except Exception as error:
+        print("Could not open config file.")
+        return None
+
+if __name__ == "__main__":
+    # Execute API requests
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", help="Enable Debug Logging")
+    parser.add_argument('--config', type=str, default='config.yml', help='Specify the location of the config file. Defaults to config.yml.')
+    args = parser.parse_args()
+    if(args.debug):
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+    myconfig = read_config(args.config)
+    
+    logging.basicConfig(filename=myconfig['logfile'], level=loglevel)
+
+    myinfluxdb = influxdb(myconfig['influxdb']['host'],
+                            myconfig['influxdb']['port'],
+                            myconfig['influxdb']['user'],
+                            myconfig['influxdb']['password'],
+                            myconfig['influxdb']['dbname'],
+                            myconfig['influxdb']['measurename'])
+    mynetatmo = netatmo(myconfig['netatmo']['authtoken'])
+    
+    logging.info('Started netatmo weather collection')
+    print("Started netatmo collection!")
+    myscheduler = sched.scheduler(time.time,time.sleep)
+    myscheduler.enter(1,1,request_nodes,(myscheduler,))
+    myscheduler.run()
+
+
